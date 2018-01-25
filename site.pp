0d960dba2a7e64b051985b25e8957fb0b103d21d
Tim Jordan <jamestjordan@gmail.com>
2018-01-25 11:49:57 -0500

    Update site.pp

--- site.pp
+++ site.pp
@@ -5,3 +5,23 @@ class install_docker {
         package_name   => 'docker-engine',
      }
 }
+class jenkins {
+  exec { 'install_jenkins_package_keys':class jenkins {
+  exec { 'install_jenkins_package_keys':
+    command => '/usr/bin/wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | /usr/bin/apt-key add - ',
+  }
+  file { "/etc/apt/sources.list.d/jenkins.list":
+     mode => 644,
+     owner => root,
+     group => root,
+     source => "puppet:///modules/jenkins/etc/apt/sources.list.d/jenkins.list",
+  }
+  package { 'jenkins':
+     ensure => latest,
+     require  => [ Exec['install_jenkins_package_keys'],             
+     File['/etc/apt/sources.list.d/jenkins.list'], ],
+  }
+  service { 'jenkins':
+     ensure => running,
+  }
+}
