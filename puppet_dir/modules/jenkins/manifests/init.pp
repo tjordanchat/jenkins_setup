cd44e813cd6804383a26a79194c68149ef32af18
Tim Jordan <jamestjordan@gmail.com>
2018-09-27 00:21:55 -0400

    Update init.pp

--- puppet_dir/modules/jenkins/manifests/init.pp
+++ puppet_dir/modules/jenkins/manifests/init.pp
@@ -1,28 +1,33 @@
 class jenkins {
- 
-  exec { 'install_jenkins_package_keys':
-    command => '/usr/bin/wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | /usr/bin/apt-key add - ',
-  }
- 
-  file { "/etc/apt/sources.list.d/jenkins.list":
-      mode => 644,
-     owner => root,
-     group => root,
-    source => "puppet:///modules/jenkins/etc/apt/sources.list.d/jenkins.list",
-  }
- 
-  package { 'jenkins':
-      ensure => latest,
-    require  => [ Exec['install_jenkins_package_keys'],
-                  File['/etc/apt/sources.list.d/jenkins.list'], ],
-  }
- 
-  service { 'jenkins':
-    ensure => running,
-  }
+    # get key
+    exec { 'install_jenkins_key':
+        command => 'wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add - ',
+    }
 
-node 'puppet.localdomain' {
-    include jenkins
-}
+    # update
+    exec { 'apt-get update':
+        command => 'apt-get update',
+        require => File['/etc /apt/sources.list.d/jenkins.list'],
+    }
 
+    # source file
+    file { '/etc /apt/sources.list.d/jenkins.list':
+        content => "deb http://pkg.jenkins-ci.org/debian binary/\n",
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        require => Exec['install_jenkins_key'],
+    }
+
+    # jenkins package
+    package { 'jenkins':
+        ensure  => latest,
+        require => Exec['apt-get update'],
+    }
+
+    # jenkins service
+    service { 'jenkins':
+        ensure  => running,
+        require => Package['jenkins'],
+    }
 }
