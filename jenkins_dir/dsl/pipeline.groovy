65e6150ab8bbf2f95e3ebee4de80865207c1e092
Tim Jordan <jamestjordan@gmail.com>
2018-09-28 18:25:04 -0400

    Update pipeline.groovy

--- jenkins_dir/dsl/pipeline.groovy
+++ jenkins_dir/dsl/pipeline.groovy
@@ -1,12 +1,4 @@
 pipelineJob('Pipeline') {
-	scm {
-        git {
-            remote {
-                name('remoteB')
-                url('git@server:account/repo1.git')
-            }
-        }
-  }
   definition {
     cps {
       sandbox()
