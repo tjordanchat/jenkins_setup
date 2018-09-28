0751a4980cec520841aeaa2ab4f77ddcac0ddfc0
Tim Jordan <jamestjordan@gmail.com>
2018-09-28 16:21:10 -0400

    Update pipeline.groovy

--- jenkins_dir/dsl/pipeline.groovy
+++ jenkins_dir/dsl/pipeline.groovy
@@ -17,13 +17,13 @@ node {
        stage 'Pull Sourse Code'
          
             echo 'Pull Source Code'
-            git url: "${Repository}"
+            git url: "\${Repository}"
 
        stage 'Parse Message'
 
             def commit = sh(script: 'git rev-parse HEAD', returnStdout: true)
-            def message = sh(script: "git log --format=%B -n 1 ${commit}", returnStdout: true)
-            def author = sh(script: "git --no-pager show -s --format='%ae' ${commit}", returnStdout: true)
+            def message = sh(script: "git log --format=%B -n 1 \${commit}", returnStdout: true)
+            def author = sh(script: "git --no-pager show -s --format='%ae' \${commit}", returnStdout: true)
             def items = message.findAll(/[A-Z]{2,10}-[0-9]+/).unique()
        
        stage 'Generate Dockerfile'
@@ -44,55 +44,55 @@ node {
        stage 'Change Resource Defintions'
        
             echo 'Change Resource Defintions'
-            sh "~/bin/g_call_process_config ${container}"
+            sh "~/bin/g_call_process_config \${container}"
         
        stage 'Run Build'
 
             echo 'Run Build'
-            sh "~/bin/g_run_build ${container}"
+            sh "~/bin/g_run_build \${container}"
 
        stage 'Take Screenshot'
 
             echo 'Take Screenshot'
-            sh "~/bin/g_call_take_screenshot ${container}"
+            sh "~/bin/g_call_take_screenshot \${container}"
 
        stage 'Run Sonar'
 
             echo  'Run Sonar'
-            sh "~/bin/g_run_sonar ${container}"
+            sh "~/bin/g_run_sonar \${container}"
 
        stage 'Run Fortify'
 
             echo 'Run Fortify'
-            sh "~/bin/g_run_fortify ${container}"
+            sh "~/bin/g_run_fortify \${container}"
             
        stage 'Update Jira'
 
             echo 'Update Jira'
             for (item in items) {
-            	sh "~/bin/g_update_jira ${item}"         
+            	sh "~/bin/g_update_jira \${item}"         
             }
 
        stage 'Archive Artifact'
 
             echo 'Archive Artifact'
-            sh "~/bin/g_call_sendto_artifactory ${container}"
+            sh "~/bin/g_call_sendto_artifactory \${container}"
 
        stage 'Extract Screenshot'
 
             echo 'Extract Screenshot'
-            sh "~/bin/g_extract_screenshot ${container}"
+            sh "~/bin/g_extract_screenshot \${container}"
 
        stage 'Send Email'
 
             echo 'Send Email'
-            sh "~/bin/g_send_email ${author}"
+            sh "~/bin/g_send_email \${author}"
             
        stage 'Kill Container'
 
             echo 'Kill Container'
-            sh "docker stop ${container}"
-            sh "docker rm ${container}"
+            sh "docker stop \${container}"
+            sh "docker rm \${container}"
 
        stage 'Remove Directory'
 
