4363d944242556aaa63d7de344b8d4de0e61caf2
Tim Jordan <jamestjordan@gmail.com>
2018-10-07 00:11:09 -0400

    Update version.py

--- python_dir/version.py
+++ python_dir/version.py
@@ -2,7 +2,7 @@ from jenkinsapi.jenkins import Jenkins
 
 def get_server_instance():
    jenkins_url = 'http://127.0.0.1:8080'
-   server = Jenkins(jenkins_url, username='foouser', password='foopassword')
+   server = Jenkins(jenkins_url, username='admin', password='foopassword')
    return server
   
 if __name__ == '__main__':
