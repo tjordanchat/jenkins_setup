f22de58af00daa9b29c2702203ccc6b6c4707118
Tim Jordan <jamestjordan@gmail.com>
2018-10-08 00:43:42 -0400

    Update do_setup.sh

--- DO/do_setup.sh
+++ DO/do_setup.sh
@@ -41,7 +41,7 @@ export JENKINS_HOME=/var/lib/jenkins
 export CDIR="$(sudo puppet config print confdir)"
 puppet apply $CDIR/manifests/site.pp
 export PASS="$( sudo cat /var/lib/jenkins/secrets/initialAdminPassword )"
-xargs java -jar $CLI -auth "admin:$PASS" -s http://127.0.0.1:8080 install-plugin < ./jenkins_setup/jenkins_dir/plugins.list
+xargs /usr/share/java -jar $CLI -auth "admin:$PASS" -s http://127.0.0.1:8080 install-plugin < ./jenkins_setup/jenkins_dir/plugins.list
 rm -rf jenkins_setup
 
 sleep 5
