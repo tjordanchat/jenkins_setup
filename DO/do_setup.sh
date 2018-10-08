bd21ae4b0aa71ec8ab33d66c4fbfabd338f7ce4c
Tim Jordan <jamestjordan@gmail.com>
2018-10-07 23:21:49 -0400

    Update do_setup.sh

--- DO/do_setup.sh
+++ DO/do_setup.sh
@@ -6,20 +6,20 @@
 export DISPLAY=':99'
 export TZ='America/New_York'
 apt-get update
-apt-get install openjdk-7-jre-headless
+apt-get -y install openjdk-7-jre-headless
 apt-get update
-apt-get install xvfb
+apt-get -y install xvfb
 apt-get update
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 dpkg --force-all -i puppetlabs-release-trusty.deb
 apt-get -y install ntp 
 apt-get update
-apt-get install puppetmaster
+apt-get -y install puppetmaster
 apt-get update
-apt-get install python-jenkinsapi
+apt-get -y install python-jenkinsapi
 apt-get update
-apt-get install puppet
+apt-get -y install puppet
 apt-get update
 puppet resource package puppetmaster ensure=latest
 puppet resource service puppetmaster ensure=running enable=true
