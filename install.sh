0df645e0204a743f6f9a0ea987f5a7376e557aa2
Tim Jordan <jamestjordan@gmail.com>
2018-10-08 17:39:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,16 +14,14 @@ apt-get update
 apt-get -y install xvfb
 apt-get update
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-#wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-#dpkg --force-all -i puppetlabs-release-trusty.deb
+wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+dpkg --force-all -i puppetlabs-release-trusty.deb
 apt-get -y install ntp 
 apt-get update
-apt-get -y install puppetmaster
-apt-get update
-apt-get -y install python-jenkinsapi
-apt-get update
-apt-get -y install puppet
-apt-get update
+#apt-get -y install puppetmaster
+#apt-get update
+#apt-get -y install puppet
+#apt-get update
 apt-get -y install openjdk-11-jre-headless
 apt-get update
 puppet resource package puppetmaster ensure=latest
@@ -41,7 +39,10 @@ cp -f ./jenkins_setup/.bashrc ./jenkins_setup/.vimrc .
 export JENKINS_HOME=/var/lib/jenkins
 #./jenkins_setup/bin/deploy_puppet
 export CDIR="$(sudo puppet config print confdir)"
-puppet apply $CDIR/manifests/site.pp
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
 sleep 5
 export PASS="$( sudo cat /var/lib/jenkins/secrets/initialAdminPassword )"
 xargs java -jar $CLI -auth "admin:$PASS" -s http://127.0.0.1:8080 install-plugin < ./jenkins_setup/jenkins_dir/plugins.list
