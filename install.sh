9e48ec24c17415ce45473f5ca3248504ae97212c
Tim Jordan <jamestjordan@gmail.com>
2018-10-11 05:10:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,7 +44,8 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get -y install jenkins
-sleep 5
+inotifywait -e close_write /var/lib/jenkins/secrets
+sleep 2
 export PASS="$( sudo cat /var/lib/jenkins/secrets/initialAdminPassword )"
 export CRUMB=$(curl -s 'http://127.0.0.1:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u admin:$PASS)
 curl -o jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
