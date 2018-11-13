902daca6d986d4d7544f2bbd4be983d7e4aa646c
Tim Jordan <jamestjordan@gmail.com>
2018-11-13 13:25:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,35 +12,35 @@ export DISPLAY=':99'
 export TZ='America/New_York'
 git clone https://github.com/tjordanchat/jenkins_setup.git
 chmod -R +rx . 
-apt-get update
-apt-get -y install openjdk-8-jre-headless
+sudo apt-get update
+sudo apt-get -y install openjdk-8-jre-headless
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/jre/bin
-apt-get update
-apt-get -y install xvfb
-apt-get update
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-dpkg --force-all -i puppetlabs-release-trusty.deb
-apt-get -y install ntp 
-apt-get update
-apt-get -y install puppetmaster
-apt-get update
-apt-get -y install puppet
-apt-get update
-apt-get -y install inotify-tools
-apt-get update
+sudo apt-get update
+sudo apt-get -y install xvfb
+sudo apt-get update
+sudo Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+sudo wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+sudo dpkg --force-all -i puppetlabs-release-trusty.deb
+sudo apt-get -y install ntp 
+sudo apt-get update
+sudo apt-get -y install puppetmaster
+sudo apt-get update
+sudo apt-get -y install puppet
+sudo apt-get update
+sudo apt-get -y install inotify-tools
+sudo apt-get update
 puppet resource package puppetmaster ensure=latest
 puppet resource service puppetmaster ensure=running enable=true
 puppet agent
-adduser --disabled-password myuser
-usermod -aG sudo myuser
-mkdir -p ~myuser/.ssh
-chown myuser ~myuser/.ssh
-chmod 700 ~myuser/.ssh
-cp ~/.ssh/authorized_keys ~myuser/.ssh
-chown myuser ~myuser/.ssh/authorized_keys
-chmod 600 ~myuser/.ssh/authorized_keys
-cp -f ./jenkins_setup/.bashrc ./jenkins_setup/.vimrc .
+sudo adduser --disabled-password myuser
+sudo usermod -aG sudo myuser
+sudo mkdir -p ~myuser/.ssh
+sudo chown myuser ~myuser/.ssh
+sudo chmod 700 ~myuser/.ssh
+sudo cp ~/.ssh/authorized_keys ~myuser/.ssh
+sudo chown myuser ~myuser/.ssh/authorized_keys
+sudo chmod 600 ~myuser/.ssh/authorized_keys
+sudo cp -f ./jenkins_setup/.bashrc ./jenkins_setup/.vimrc .
 export JENKINS_HOME=/var/lib/jenkins
 #./jenkins_setup/bin/deploy_puppet
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
@@ -48,7 +48,7 @@ sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sour
 sudo apt-get update
 sudo apt-get -y install jenkins
 sudo touch /var/lib/jenkins/secrets/initialAdminPassword
-inotifywait -e close_write /var/lib/jenkins/secrets
+sudo inotifywait -e close_write /var/lib/jenkins/secrets
 sudo add-apt-repository ppa:rmescandon/yq
 sudo apt update
 sudo apt install yq -y
@@ -66,7 +66,7 @@ su -c "cp /root/jenkins_setup/jenkins_dir/jobs/config.xml /var/lib/jenkins/jobs/
 #rm -rf jenkins_setup
 
 ps -ef | egrep jenkins
-netstat -tunpl
+sudo netstat -tunpl
 cat /etc/passwd
 echo $PASS
 ifconfig eth0 | egrep inet 
