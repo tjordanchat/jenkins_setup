e9599d98ba748c02c14f947867af213231dd0bb3
Tim Jordan <jamestjordan@gmail.com>
2018-12-09 22:02:44 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -80,5 +80,5 @@ ps -ef | egrep jenkins
 sudo netstat -tunpl
 cat /etc/passwd
 ifconfig eth0 | egrep inet
-#curl -v -I -u adminn:$PASS 'http://127.0.0.1:8080/job/seed/buildWithParameters?token=phoenix&URL=myURL'
-java -jar ./jenkins-cli.jar -auth 'myuser:mypass' -s http://localhost:8080 build seed -p URL=myURL
+curl -v -I -u admin:$PASS 'http://127.0.0.1:8080/job/seed/buildWithParameters?token=phoenix&URL=myURL'
+java -jar ./jenkins-cli.jar -auth "admin:$PASS" -s http://localhost:8080 build seed -p URL=myURL
