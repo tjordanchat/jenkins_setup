775b4c8060d7c2abc99df09f5b706a400b27348b
James Jordan <jamestjordan@gmail.com>
2019-02-24 02:47:15 -0500

    Sun Feb 24 02:44:13 EST 2019

--- installer.sh
+++ installer.sh
@@ -82,5 +82,6 @@ ps -ef | egrep jenkins
 sudo netstat -tunpl
 cat /etc/passwd
 ifconfig eth0 | egrep inet
+travis setup releases
 #curl -v -I -u admin:$PASS 'http://127.0.0.1:8080/job/seed/buildWithParameters?token=phoenix&URL=myURL'
 #java -jar ./jenkins-cli.jar -auth "admin:$PASS" -s http://localhost:8080 build seed -p URL=myURL
