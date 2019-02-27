a6aa6f34a0f41ec35a32c6896d7e962208ce151c
James Jordan <jamestjordan@gmail.com>
2019-02-27 05:27:44 -0500

    Tue Feb 26 23:58:14 EST 2019

--- installer.sh
+++ installer.sh
@@ -74,7 +74,7 @@ sudo chown jenkins /var/lib/jenkins/jobs/seed
 sudo chown jenkins /var/lib/jenkins/jobs/seed/config.xml
 sudo /etc/init.d/jenkins restart
 
-google-chrome-stable http://localhost:8080 &
+google-chrome-stable --no-first-run http://localhost:8080 &
 #google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 http://localhost:8080 &
 xterm -geometry 80x24+30+200 &
 xclock -geometry 48x48-0+0 &
