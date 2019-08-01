cef719f1b82d94ed5ed10eb424e6d727b334e221
Tim Jordan <jamestjordan@gmail.com>
2019-08-01 14:26:14 -0400

    Update installer2.sh

--- installer2.sh
+++ installer2.sh
@@ -146,6 +146,8 @@ Update_Package_Manager
 #Install_Ruby
 Install_Java
 Install_Misc_Tools
+Install_Virtual_Frame_Buffer
+Run_Virtual_Frame_Buffer
 Run_Jenkins
 
 ####################################
