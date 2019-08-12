fdbaf7a937d30978b33c9dd9f404b4ecd485bb29
James Jordan <jamestjordan@gmail.com>
2019-08-12 15:33:00 -0400

    Mon Aug 12 14:15:04 EDT 2019

--- installer2.sh
+++ installer2.sh
@@ -18,7 +18,7 @@ sudo apt-get -y install toilet figlet
 
 ----- $TRAVIS_COMMIT
 
-find . -name initialAdminPassword 
+sudo find / -name initialAdminPassword 
 
 ###################################
 ----- DEFINE VARIABLES
