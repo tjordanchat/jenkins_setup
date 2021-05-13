6fc7cdf96d07ce4bd38a7f854265572fc61fdb42
Tim Jordan <jamestjordan@gmail.com>
2021-05-13 14:37:04 -0400

    Wed May 12 13:33:33 EDT 2021

--- installer2.sh
+++ installer2.sh
@@ -130,7 +130,8 @@ Run_Build () {
    xclock -geometry 48x48-0+0 &
    xbiff -geometry 48x48-48+0 &
    #sudo ln -s /var/lib/dbus/machine-id /etc/machine-id
-   sudo apt install ./google-chrome-stable_current_amd64.deb
+   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+   sudo apt-get install ./google-chrome-stable_current_amd64.deb
    google-chrome-stable --no-first-run http://127.0.0.1:8080/me/my-views/view/all/ &
    curl -o jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
 }
