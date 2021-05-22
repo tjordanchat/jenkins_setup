3ad7441d088dc63d80001f766e73f34404c9243f
Tim Jordan <jamestjordan@gmail.com>
2021-05-22 18:00:15 -0400

    Tue May 18 12:55:19 EDT 2021

--- installer2.sh
+++ installer2.sh
@@ -182,7 +182,8 @@ Run_Jenkins
 Run_Applications
 Take_Screenshot
 
-export CRUMB=$(curl -s 'http://127.0.0.1:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u admin:$PASS)
+export CRUMB=$(curl -s 'http://127.0.0.1:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+#export CRUMB=$(curl -s 'http://127.0.0.1:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u admin:$PASS)
 
 Install_Jenkins_Plugins
 
