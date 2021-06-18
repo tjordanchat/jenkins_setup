2592c92dc4d01986f4063bb5b761612015f62dda
Tim Jordan <jamestjordan@gmail.com>
2021-06-18 13:21:54 -0400

    Fri Jun 18 08:26:24 EDT 2021

--- installer2.sh
+++ installer2.sh
@@ -107,7 +107,7 @@ Run_Jenkins () {
 Install_Jenkins_Plugins () {
    ----- INSTALL JENKINS PLUGINS
    #python $MYHOME/python_dir/version.py "$PASS"
-   curl -o jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
+   #curl -o jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
    #curl -H "$CRUMB" --data-urlencode -d script="$(<$MYHOME/groovy_dir/all_jobs.gsh)" http://127.0.0.1:8080/scriptText
    sudo xargs java -jar ./jenkins-cli.jar -auth "admin:$PASS" -s http://127.0.0.1:8080 install-plugin < $MYHOME/jenkins_dir/plugins.list
 }
@@ -118,6 +118,7 @@ Run_Build () {
 
 Install_Initial_Jenkins_Jobs () {
    ----- INSTALL INITIAL JENKINS JOBS
+   curl -o jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
    sudo java -jar ./jenkins-cli.jar -auth "admin:$PASS" -s http://127.0.0.1:8080  create-job seed  < $MYHOME/jenkins_dir/jobs/config.xml
    sudo find /var/lib/jenkins/jobs -ls
 }
