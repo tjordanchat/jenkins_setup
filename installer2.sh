d9d9248f5d97abeb669f4a6ad962d11ad686e586
Tim Jordan <jamestjordan@gmail.com>
2021-06-18 11:51:47 -0400

    Fri Jun 18 08:26:24 EDT 2021

--- installer2.sh
+++ installer2.sh
@@ -16,7 +16,7 @@ set -v -x -e
 
 sudo apt-get -f install
 sudo apt-get update -y 2>/dev/null
-sudo apt-get install -y toilet
+sudo apt-get install -y toilet toilet-fonts
 sudo apt-get -f install figlet -y
 
 ----- `pwd`
