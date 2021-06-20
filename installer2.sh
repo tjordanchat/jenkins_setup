2cd275dd339d6f6fd4eb721678bd1f5b98d06635
Tim Jordan <jamestjordan@gmail.com>
2021-06-20 10:15:52 -0400

    Fri Jun 18 13:54:39 EDT 2021

--- installer2.sh
+++ installer2.sh
@@ -120,7 +120,8 @@ Install_Initial_Jenkins_Jobs () {
    ----- INSTALL INITIAL JENKINS JOBS
    curl -o jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
    sudo mkdir -p /var/lib/jenkins/jobs/seed
-   sudo java -Dorg.xml.sax.driver=com.sun.org.apache.xerces.internal.parsers.SAXParser -jar ./jenkins-cli.jar -auth "admin:$PASS" -s http://127.0.0.1:8080  create-job seed  < $MYHOME/jenkins_dir/jobs/config.xml
+   sudo cp $MYHOME/jenkins_dir/jobs/config.xml /var/lib/jenkins/jobs/seed
+   #sudo java -Dorg.xml.sax.driver=com.sun.org.apache.xerces.internal.parsers.SAXParser -jar ./jenkins-cli.jar -auth "admin:$PASS" -s http://127.0.0.1:8080  create-job seed  < $MYHOME/jenkins_dir/jobs/config.xml
 }
 
 Install_Misc_Tools () {
