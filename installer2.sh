33d97cfb52123007bf463c16dcf49437b57d8cc2
Tim Jordan <jamestjordan@gmail.com>
2021-06-21 10:41:42 -0400

    Mon Jun 21 10:13:08 EDT 2021

--- installer2.sh
+++ installer2.sh
@@ -123,7 +123,7 @@ Install_Initial_Jenkins_Jobs () {
    ----- INSTALL INITIAL JENKINS JOBS
    #curl -o jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
    export JPID="$( cat .jpid )"
-   kill -9 $JPID
+   sudo kill -9 $JPID
    sudo mkdir -p /var/lib/jenkins/jobs/seed
    sudo chmod 777 /var/lib/jenkins/jobs/seed
    sudo cp $MYHOME/jenkins_dir/jobs/config.xml /var/lib/jenkins/jobs/seed
